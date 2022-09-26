c8d61d40ab7573f22be4fc9444fac073de560513
yqlwqj <yqlwqj@hotmail.com>
2022-09-26 17:56:20 +0800

    Update spatial.py

--- stnet/datasets/spatial.py
+++ stnet/datasets/spatial.py
@@ -1,203 +1,250 @@
-from functools import partial
-
 import os
-import os.path as osp
-
-from typing import Any, Callable, Optional, NamedTuple, Type
-
-import yaml
-
 import numpy as np
-
-import pandas as pd
-
-from PIL.Image import open as read_image
-
-import torch as t
-from torch.utils.data import DataLoader, Dataset
-
-from torchvision import transforms
-
-import stnet
-
-import torch
-
-import PIL
-import torchvision
-import glob
-import openslide
 import pickle
 import logging
 import pathlib
-import statistics
+import datetime
+import time
+import glob
 import collections
+import stnet
+import tqdm
+import pandas as pd
 
-# TODO: make this a visiondataset
-class Spatial(torch.utils.data.Dataset):
-    def __init__(self,
-                 patient=None,
-                 transform=None,
-                 window=224,
-                 cache=False,
-                 root=stnet.config.SPATIAL_PROCESSED_ROOT,
-                 gene_filter="tumor",
-                 load_image=True,
-                 gene_transform="log",
-                 downsample=1,
-                 norm=None,
-                 feature=False):
+def spatial(args):
+    import skimage.io
 
-        self.dataset = sorted(glob.glob("{}/*/*/*.npz".format(root)))
-        if patient is not None:
-            # Can specify (patient, section) or only patient (take all sections)
-            self.dataset = [d for d in self.dataset if ((d.split("/")[-2] in patient) or ((d.split("/")[-2], d.split("/")[-1].split("_")[0]) in patient))]
+    window = 224  # only to check if patch is off of boundary
+    stnet.utils.logging.setup_logging(args.logfile, args.loglevel)
 
-            # TODO: if patient == [], then many things downstream bug out
-            # how to handle this case?
-            # Could just throw an error
+    logger = logging.getLogger(__name__)
 
-        # TODO: filter things that are too close to edge?
+    pathlib.Path(args.dest).mkdir(parents=True, exist_ok=True)
 
-        self.transform = transform
-        self.window = window
-        self.downsample = downsample
-        self.cache = cache
-        self.root = root
-        self.load_image = load_image
-        self.gene_transform = gene_transform
-        self.norm = norm
-        self.feature = feature
+    raw, subtype = load_raw(args.root)
 
-        with open(root + "/subtype.pkl", "rb") as f:
-            self.subtype = pickle.load(f)
-        with open(root + "/gene.pkl", "rb") as f:
-            self.ensg_names = pickle.load(f)
+    with open(args.dest + "/subtype.pkl", "wb") as f:
+        pickle.dump(subtype, f)
 
-        self.gene_names = list(map(lambda x: stnet.utils.ensembl.symbol[x], self.ensg_names))
-        self.mean_expression = np.load(stnet.config.SPATIAL_PROCESSED_ROOT + "/mean_expression.npy")
-        self.median_expression = np.load(stnet.config.SPATIAL_PROCESSED_ROOT + "/median_expression.npy")
+    t = time.time()
 
-        self.slide = collections.defaultdict(dict)
-        # TODO: this can be parallelized
-        for (patient, section) in set([(d.split("/")[-2], d.split("/")[-1].split("_")[0]) for d in self.dataset]):
-            self.slide[patient][section] = openslide.open_slide("{}/{}/{}/{}_{}.tif".format(stnet.config.SPATIAL_RAW_ROOT, self.subtype[patient], patient, patient, section))
+    t0 = time.time()
+    section_header = None
+    gene_names = set()
+    for patient in raw:
+        for section in raw[patient]:
+            section_header = raw[patient][section]["count"].columns.values[0]
+            gene_names = gene_names.union(set(raw[patient][section]["count"].columns.values[1:]))
+    gene_names = list(gene_names)
+    gene_names.sort()
+    with open(args.dest + "/gene.pkl", "wb") as f:
+        pickle.dump(gene_names, f)
+    gene_names = [section_header] + gene_names
+    logger.info("Finding list of genes: " + str(time.time() - t0))
 
-        if gene_filter is None or gene_filter == "none":
-            self.gene_filter = None
-        elif gene_filter == "high":
-            self.gene_filter = np.array([m > 1. for m in self.mean_expression])
-        elif gene_filter == "tumor":
-            # These are the 10 genes with the largest difference in expression between tumor and normal tissue
-            # Printed by save_counts.py
-            self.tumor_genes = ["PABPC1", "GNAS", "HSP90AB1", "TFF3", "ATP1A1", "COX6C", "B2M", "FASN", "ACTG1", "HLA-B"]
-            self.gene_filter = np.array([g in self.tumor_genes for g in self.gene_names])
-        elif isinstance(gene_filter, list):
-            self.gene_filter = np.array([g in gene_filter or e in gene_filter for (g, e) in zip(self.gene_names, self.ensg_names)])
-        elif isinstance(gene_filter, int):
-            keep = set(list(zip(*sorted(zip(self.mean_expression, range(self.mean_expression.shape[0])))[::-1][:gene_filter]))[1])
-            self.gene_filter = np.array([i in keep for i in range(len(self.gene_names))])
+    for (i, patient) in enumerate(raw):
+        logger.info("Processing " + str(i + 1) + " / " + str(len(raw)) + ": " + patient)
+
+        for section in raw[patient]:
+            pathlib.Path("{}/{}/{}".format(args.dest, subtype[patient], patient)).mkdir(parents=True, exist_ok=True)
+
+            # This is just a blank file to indicate that the section has been completely processed.
+            # Preprocessing occassionally crashes, and this lets the preparation restart from where it let off
+            complete_filename = "{}/{}/{}/.{}".format(args.dest, subtype[patient], patient, section)
+            if pathlib.Path(complete_filename).exists():
+                logger.info("Patient {} section {} has already been processed.".format(patient, section))
+            else:
+                logger.info("Processing " + patient + " " + section + "...")
+
+                # In the original data, genes with no expression in a section are dropped from the table.
+                # This adds the columns back in so that comparisons across the sections can be done.
+                t0 = time.time()
+                missing = list(set(gene_names) - set(raw[patient][section]["count"].keys()))
+                c = raw[patient][section]["count"].values[:, 1:].astype(float)
+                pad = np.zeros((c.shape[0], len(missing)))
+                c = np.concatenate((c, pad), axis=1)
+                names = np.concatenate((raw[patient][section]["count"].keys().values[1:], np.array(missing)))
+                c = c[:, np.argsort(names)]
+                logger.info("Adding zeros and ordering columns: " + str(time.time() - t0))
+
+                t0 = time.time()
+                count = {}
+                for (j, row) in raw[patient][section]["count"].iterrows():
+                    count[row.values[0]] = c[j, :]
+                logger.info("Extracting counts: " + str(time.time() - t0))
+
+                t0 = time.time()
+                tumor = {}
+                not_int = False
+                for (_, row) in raw[patient][section]["tumor"].iterrows():
+                    if isinstance(row[1], float) or isinstance(row[2], float):
+                        not_int = True
+                    tumor[(int(round(row[1])), int(round(row[2])))] = (row[4] == "tumor")
+                if not_int:
+                    logger.warning("Patient " + patient + " " + section + " has non-integer patch coordinates.")
+                logger.info("Extracting tumors: " + str(time.time() - t0))
+
+                t0 = time.time()
+                image = skimage.io.imread(raw[patient][section]["image"])
+                logger.info("Loading image: " + str(time.time() - t0))
+
+                data = []
+                for (_, row) in raw[patient][section]["spot"].iterrows():
+                    x = int(round(row['X']))
+                    y = int(round(row['Y']))
+
+#                   tumor_list = pd.DataFrame(list(tumor.items()))
+                    tumor_list = pd.DataFrame(list(tumor.items()), columns=["1","2"])
+
+                    X = image[(y + (-window // 2)):(y + (window // 2)), (x + (-window // 2)):(x + (window // 2)), :]
+
+                    row_list = pd.DataFrame(list(row.items()))
+
+                    if X.shape == (window, window, 3):
+                        a = row["Unnamed: 0"].replace('x',', ')
+                        a = '(' + a +')'
+                        b = eval(a) 
+
+                        if tuple(b) in tumor:
+                            data.append((X,
+#                                        count[str(int(row["x"])) + "x" + str(int(row["y"]))],
+#              				 tumor[(int(row["x"]), int(row["y"]))],
+#					 np.array([x, y]),
+#                                        np.array([patient]),
+#                                        np.array([section]),
+#                                        np.array([int(row["x"]), int(row["y"])]),
+#                                        ))
+                                         count[row["Unnamed: 0"]],
+              				 tumor[b],
+					 np.array([x, y]),
+                                         np.array([patient]),
+                                         np.array([section]),
+                                         np.array([int(row["X"]), int(row["Y"])]),
+                                         ))
+
+
+#                           filename = "{}/{}/{}/{}_{}_{}.npz".format(args.dest, subtype[patient], patient, section,
+#                                                                     int(row["x"]), int(row["y"]))
+                            filename = "{}/{}/{}/{}_{}_{}.npz".format(args.dest, subtype[patient], patient, section,
+                                                                      int(row["X"]), int(row["Y"]))
+
+
+#                           np.savez_compressed(filename, count=count[str(int(row["x"])) + "x" + str(int(row["y"]))],
+#                                               tumor=tumor[(int(row["x"]), int(row["y"]))],
+#                                               pixel=np.array([x, y]),
+#                                               patient=np.array([patient]),
+#                                               section=np.array([section]),
+#                                               index=np.array([int(row["x"]), int(row["y"])]))
+                            np.savez_compressed(filename, count=count[row["Unnamed: 0"]],
+                                                tumor=tumor[b],
+                                                pixel=np.array([x, y]),
+                                                patient=np.array([patient]),
+                                                section=np.array([section]),
+                                                index=np.array([int(row["X"]), int(row["Y"])]))
+
+                        else:
+#                           logger.warning("Patch " + str(int(row["x"])) + "x" + str(
+#                               int(row["y"])) + " not found in " + patient + " " + section)
+                            logger.warning("Patch " + row["Unnamed: 0"]
+                                 + " not found in " + patient + " " + section)
+                    else:
+                        logger.warning("Detected spot too close to edge.")
+                logger.info("Saving patches: " + str(time.time() - t0))
+
+                with open(complete_filename, "w"):
+                    pass
+    logger.info("Preprocessing took " + str(time.time() - t) + " seconds")
+
+    if (not os.path.isfile(stnet.config.SPATIAL_PROCESSED_ROOT + "/mean_expression.npy") or
+        not os.path.isfile(stnet.config.SPATIAL_PROCESSED_ROOT + "/median_expression.npy")):
+        logging.info("Computing statistics of dataset")
+        gene = []
+        for filename in tqdm.tqdm(glob.glob("{}/*/*/*_*_*.npz".format(args.dest))):
+            npz = np.load(filename)
+            count = npz["count"]
+            gene.append(np.expand_dims(count, 1))
+
+        gene = np.concatenate(gene, 1)
+        np.save(stnet.config.SPATIAL_PROCESSED_ROOT + "/mean_expression.npy", np.mean(gene, 1))
+        np.save(stnet.config.SPATIAL_PROCESSED_ROOT + "/median_expression.npy", np.median(gene, 1))
+
+
+def load_section(root: str, patient: str, section: str, subtype: str):
+    """
+    Loads data for one section of a patient.
+    """
+    import pandas
+    import gzip
+
+    #file_root = root + "/" + subtype + "/" + patient + "/" + patient + "_" + section
+    file_root = root + subtype + "/" + patient + "/" + patient + "_" + section
+
+    # image = skimage.io.imread(file_root + ".jpg")
+    image = file_root + ".jpg"
+
+    if stnet.utils.util.newer_than(file_root + ".tsv.gz", file_root + ".pkl"):
+        with gzip.open(file_root + ".tsv.gz", "rb") as f:
+            count = pandas.read_csv(f, sep="\t")
+        with open(file_root + ".pkl", "wb") as f:
+            pickle.dump(count, f)
+    else:
+        with open(file_root + ".pkl", "rb") as f:
+            count = pickle.load(f)
+
+    if stnet.utils.util.newer_than(file_root + ".spots.txt.gz", file_root + ".spots.pkl"):
+        with gzip.open(file_root + ".spots.txt.gz", "rb") as f:
+#           spot = pandas.read_csv(f, sep="\t")
+            spot = pandas.read_csv(f, sep=",")
+        with open(file_root + ".spots.pkl", "wb") as f:
+            pickle.dump(spot, f)
+    else:
+        with open(file_root + ".spots.pkl", "rb") as f:
+            spot = pickle.load(f)
+
+    if stnet.utils.util.newer_than(file_root + "_Coords.tsv.gz", file_root + "_Coords.pkl"):
+        with gzip.open(file_root + "_Coords.tsv.gz", "rb") as f:
+            tumor = pandas.read_csv(f, sep="\t")
+        with open(file_root + "_Coords.pkl", "wb") as f:
+            pickle.dump(tumor, f)
+    else:
+        with open(file_root + "_Coords.pkl", "rb") as f:
+            tumor = pickle.load(f)
+
+    return {"image": image, "count": count, "spot": spot, "tumor": tumor}
+
+def load_raw(root: str):
+    """
+    Loads data for all patients.
+    """
+
+    logger = logging.getLogger(__name__)
+
+    # Wildcard search for patients/sections
+    images = glob.glob(root + "/*/*/*_*.jpg")
+
+    # Dict mapping patient ID (str) to a list of all sections available for the patient (List[str])
+    patient = collections.defaultdict(list)
+    for (p, s) in map(lambda x: x.split("/")[-1][:-4].split("_"), images):
+        patient[p].append(s)
+
+    # Dict mapping patient ID (str) to subtype (str)
+    subtype = {}
+    for (st, p) in map(lambda x: (x.split("/")[-3], x.split("/")[-1][:-4].split("_")[0]), images):
+        if p in subtype:
+            if subtype[p] != st:
+                raise ValueError("Patient {} is marked as type {} and {}.".format(p, subtype[p], st))
         else:
-            raise ValueError()
+            subtype[p] = st
 
-        if self.gene_filter is not None:
-            self.ensg_names = [n for (n, f) in zip(self.ensg_names, self.gene_filter) if f]
-            self.gene_names = [n for (n, f) in zip(self.gene_names, self.gene_filter) if f]
-            self.mean_expression = self.mean_expression[self.gene_filter]
-            self.median_expression = self.median_expression[self.gene_filter]
+    logger.info("Loading raw data...")
+    t = time.time()
+    data = {}
+    with tqdm.tqdm(total=sum(map(len, patient.values()))) as pbar:
+        for p in patient:
+            data[p] = {}
+            for s in patient[p]:
+                data[p][s] = load_section(root, p, s, subtype[p])
+                pbar.update()
+    logger.info("Loading raw data took " + str(time.time() - t) + " seconds.")
 
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, index):
-
-        npz = np.load(self.dataset[index])
-
-        count   = npz["count"]
-        tumor   = npz["tumor"]
-        pixel   = npz["pixel"]
-        patient = npz["patient"][0]
-        section = npz["section"][0]
-        coord   = npz["index"]
-
-        feature_filename = os.path.splitext(self.dataset[index])[0] + "_feature.npy"
-        need_image = self.feature and not os.path.isfile(feature_filename)
-
-        if self.load_image or need_image:
-            orig_window = self.window * self.downsample
-            cached_image = "{}/{}/{}/{}_{}_{}_{}_{}_{}.tif".format(self.root, self.subtype[patient], patient, patient, section, orig_window, self.downsample, coord[0], coord[1])
-            if self.cache and pathlib.Path(cached_image).exists():
-                X = PIL.Image.open(cached_image)
-            else:
-                slide = self.slide[patient][section]
-                X = slide.read_region((pixel[0] - orig_window // 2, pixel[1] - orig_window // 2), 0, (orig_window, orig_window))
-                X = X.convert("RGB")
-
-                if self.cache:
-                    X.save(cached_image)
-
-                # TODO: check downsample
-                if self.downsample != 1:
-                    X = torchvision.transforms.Resize((self.window, self.window))(X)
-
-            if self.transform is not None:
-                X = self.transform(X)
-
-        if self.feature:
-            try:
-                f = np.load(feature_filename)
-            except FileNotFoundError:
-                asd
-                f = stnet.util.histology.features(X).numpy()
-                np.save(feature_filename, f)
-            f = torch.Tensor(f[0, :])
-        else:
-            f = []
-
-        if not self.load_image:
-            X = []
-
-        Z = np.sum(count)
-        n = count.shape[0]
-        if self.gene_filter is not None:
-            count = count[self.gene_filter]
-        y = torch.as_tensor(count, dtype=torch.float)
-
-        tumor = torch.as_tensor([1 if tumor else 0])
-        coord = torch.as_tensor(coord)
-        index = torch.as_tensor([index])
-
-        if self.norm is None or self.norm == "none":
-            if self.gene_transform == "log":
-                y = torch.log(1 + y)
-            elif self.gene_transform == "sqrt":
-                y = torch.sqrt(y)
-            else:
-                assert(self.gene_transform is None or self.gene_transform == "none")
-        elif self.norm == "norm":
-            if self.gene_transform == "log":
-                y = torch.log((1 + y) / (n + Z))
-            else:
-                assert(self.gene_transform is None or self.gene_transform == "none")
-                y = y / Z
-        elif self.norm == "normfilter":
-            if self.gene_transform == "log":
-                y = torch.log((1 + y) / torch.sum(1 + y))
-            else:
-                assert(self.gene_transform is None or self.gene_transform == "none")
-                y = y / torch.sum(y)
-        elif self.norm == "normpat":
-            if self.gene_transform == "log":
-                y = torch.log((1 + y) / self.p_median[patient])
-            else:
-                assert(self.gene_transform is None or self.gene_transform == "none")
-                y = y / self.ps_median[(patient, section)]
-        elif self.norm == "normsec":
-            if self.gene_transform == "log":
-                y = torch.log((1 + y) / self.ps_median[(patient, section)])
-            else:
-                assert(self.gene_transform is None or self.gene_transform == "none")
-                y = y / self.ps_median[(patient, section)]
-        else:
-            raise ValueError()
-
-        return X, tumor, y, coord, index, patient, section, pixel, f
+    return data, subtype
