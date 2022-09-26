3e38e349c7b25a77cad14c94c995a1784c6f1dd6
yqlwqj <yqlwqj@hotmail.com>
2022-09-26 17:59:42 +0800

    Update create_tifs.sh

--- bin/create_tifs.sh
+++ bin/create_tifs.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-cd `python -c "import histonet; print(histonet.config.SPATIAL_RAW_ROOT)"`
-for i in */*/*.jpg;
-do
-    echo ${i}
-    convert ${i} -define tiff:tile-geometry=256x256 ${i%.jpg}.tif
-done
+#cd `python -c "import histonet; print(histonet.config.SPATIAL_RAW_ROOT)"`
+#for i in */*/*.jpg;
+#do
+#    echo ${i}
+#    convert ${i} -define tiff:tile-geometry=256x256 ${i%.jpg}.tif
+#done
+#!/bin/bash
+
+path="/home/ben/project"
+
+# Search .jpg file in all directory
+
+function readJpg ()
+{
+	for file in `ls $1`
+	do
+	if [ -d $1"/"$file ]
+	then
+		readJpg $1"/"$file
+	else
+
+		if echo $1"/"$file | grep -q -E '.jpg'
+		then
+		i="$1"/"$file"
+			echo $i
+			convert $i -define tiff:tile-geometry=256x256 ${i%.jpg}.tif
+		fi
+	fi
+	done
+}
+
+readJpg $path
